84930693a321e91deb11b375c2b1c7397e7272eb
KRP <kunal.r.rajpurohit@gmail.com>
2020-12-18 14:18:30 -0800

    ci script to push altered services

--- ci/get_modified_services.sh
+++ ci/get_modified_services.sh
@@ -5,6 +5,19 @@ cd "$(git rev-parse --show-toplevel)"
 
 # Get a list of the current files in package form by querying Bazel.
 files=()
+echo "STEP 1: Files -> SRC File Targets"
 for file in $(git diff --name-only ${COMMIT_RANGE} ); do
-  echo $(bazel query $file)
+   files+=($(bazel query --noshow_progress $file))
+done
+
+modified_push_targets=()
+echo "STEP 2: Retrieve Push Targets"
+for file in ${files[*]}; do
+   modified_push_targets+=($(bazel query --noshow_progress "kind(".*push.*", rdeps(//..., set(${file})))"))
+done
+
+echo "STEP 3: Running Push Targets"
+for target in ${modified_push_targets[*]}; do
+   $(bazel run ${target})
+   echo "DONE: ${target}" 
 done
