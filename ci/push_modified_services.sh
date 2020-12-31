8bbccb42bff7041579d70af51b06b4665ca7a93b
KRP <kunal.r.rajpurohit@gmail.com>
2020-12-30 16:22:38 -0800

    ci: tags are static & check files changed in last commit for main

--- ci/push_modified_services.sh
+++ ci/push_modified_services.sh
@@ -1,12 +1,10 @@
-COMMIT_RANGE=${COMMIT_RANGE:-$(git merge-base origin/main HEAD)".."}
-
 # Go to the root of the repo
 cd "$(git rev-parse --show-toplevel)"
 
 # Get a list of the current files in package form by querying Bazel.
 files=()
 echo "STEP 1: Files -> SRC File Targets"
-for file in $(git diff --name-only ${COMMIT_RANGE} ); do
+for file in $(git diff --name-only HEAD HEAD~1 ); do
    files+=($(bazel query --noshow_progress $file))
 done
 
@@ -18,7 +16,6 @@ done
 
 echo "STEP 3: Running Push Targets"
 for target in ${modified_push_targets[*]}; do
-   $(SERVICE_TARGET=${target} bazel run //ci:alter_service_config)
    $(bazel run ${target})
    echo "DONE: ${target}" 
 done
--- ci/service_configs.bzl
+++ ci/service_configs.bzl
@@ -1,11 +1,8 @@
 ECR_URL="534378753229.dkr.ecr.us-east-2.amazonaws.com/action-bazeldemo"
 
-ACTION_TAG="1.3"
 ACTION_REPO="action-bazeldemo"
 
-POLL_TAG="1.1"
 POLL_REPO="poll-bazeldemo"
 
-GATEWAY_TAG="1.2"
 GATEWAY_REPO="gateway-bazeldemo"
 GATEWAY_BUILD_TAGS=[]
